e3be1faafe48708ccd301e2141b655b512d3518b
italieno <italieno@gmail.com>
2014-11-22 21:35:37 +0000

    Creating New Projects to host domain model services and tests

---
+++ SPA.Integration.Test/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SPA.Integration.Test
+{
+    public class Class1
+    {
+    }
+}
