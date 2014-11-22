ed372d7a373884a64c051e6f6e2d57d3103b783b
italieno <italieno@gmail.com>
2014-11-22 22:11:53 +0000

    added service installer

---
+++ SPA.Web/Installers/ServiceInstaller.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+using SPA.Infra.Services;
+
+namespace SPA.Web.Installers
+{
+    public class ServiceInstaller : IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            container.Register(
+                Component.For<IDoSomethingService>().ImplementedBy<DoSomethingService>()
+                );
+        }
+    }
+}
