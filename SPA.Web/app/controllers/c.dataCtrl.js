1269b2f0e171220dd580e08320a4351cc76dd6ed
italieno <italieno@gmail.com>
2014-11-23 12:30:00 +0000

    adding ui-router management fro multiple app states

---
+++ SPA.Web/app/controllers/c.dataCtrl.js
@@ -0,0 +1,17 @@
+﻿spAngular.controller('dataCtrl', ['$scope', '$log', 'dataService', function ($scope, $log, dataService) {
+	$scope.welcomeMessage = "Hello SPAngular";
+	$scope.items = [];
+
+	$scope.doSomething = function () {
+
+		var promise = dataService.getData();
+		promise.then(
+		   function (payload) {
+		   	$scope.items = payload;
+		   },
+		   function (errorPayload) {
+		   	$log.error('failure loading data', errorPayload);
+		   });
+
+	}
+}]);
---
+++ SPA.Web/app/services/s.dataService.js
@@ -0,0 +1,23 @@
+﻿spAngular.factory('dataService', function ($http, $log, $q, blockUI) {
+  return {
+    getData: function () {
+
+      blockUI.start();
+
+      var deferred = $q.defer();
+
+      $http.get('/api/someprefix/items')
+        .success(function (data) {
+          blockUI.stop();
+          deferred.resolve(data);
+        }).error(function (msg, code) {
+          blockUI.stop();
+          deferred.reject(msg);
+          $log.error(msg, code);
+        });
+
+
+      return deferred.promise;
+    }
+  }
+});
